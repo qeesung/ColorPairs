116b5f2a839373d0ae803ec5d1465af745f56c95
qeesung <1245712564@qq.com>
2015-11-26 21:40:45 +0800

    add some basic files that belong to intllij project

---
+++ src/io/github/qeesung/actions/ColorPairAction.java
@@ -0,0 +1,16 @@
+package io.github.qeesung.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by qeesung on 2015/11/26.
+ * an action that can triggle the color pair
+ */
+public class ColorPairAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
