b4a48dea66648a4376a743c8639c509e9e9f0a33
qeesung <1245712564@qq.com>
2015-11-28 00:30:26 +0800

    add ColorPairs Configuration action and add them to tool menu as a group

---
+++ src/io/github/qeesung/actions/ColorPairsConfiguration.java
@@ -0,0 +1,15 @@
+package io.github.qeesung.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by qeesung on 2015/11/28.
+ */
+public class ColorPairsConfiguration extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
